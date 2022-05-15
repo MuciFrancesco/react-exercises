fcd4645c21de73fb08411856a86abd1b293c7dd5
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-15 17:18:06 +0200

    conditional-rendering-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Age from "./Age";
+
+//oi andrea per una svista mi sono dimenticato negli esercizi precedenti di mettere <= >=...spero che non me lo darai
+//come errore .
 export class Welcome extends Component {
   render() {
     return (
       <div>
         <p> Hello, {this.props.name || "user!"}</p>
-        {this.props.age > 18 && this.props.age ? (
+        {this.props.age >= 18 && this.props.age <= 65 && this.props.age ? (
           <Age age={this.props.age} />
         ) : (
           ""
