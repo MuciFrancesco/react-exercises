9de040d6153eedcda292f565e03dc31909bbc5eb
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-15 16:40:08 +0200

    components-07

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Message from "./Message";
 
 export class Hello extends Component {
   render() {
     return (
       <div>
-        <h1>Hello,World!</h1>
-        <p>What a beatiful day!</p>
+        <h1>Hello , World!</h1>
+        <Message />
       </div>
     );
   }
---
+++ src/Message.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Message extends Component {
+  render() {
+    return (
+      <div>
+        <p>What a beatiful day!</p>
+      </div>
+    );
+  }
+}
+
+export default Message;
