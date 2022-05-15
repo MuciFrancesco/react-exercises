5609cec9e12056ec82ba8c68f1540de4a1943dc1
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-15 17:01:21 +0200

    props-04

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 
+const name = <strong>Andrea</strong>;
 export class App extends Component {
   render() {
     return (
       <div>
         <Hello />
-        <Welcome name='andrea' age='22' />
+        <Welcome name={name} age='22' />
       </div>
     );
   }
