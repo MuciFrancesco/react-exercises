3389553cbb4b93da2e8c2b2ae237a35eb0b10e06
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-17 02:30:10 +0200

    events-03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hello from "./Hello";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
 import ClickCounter from "./ClickCounter";
+import ClickTracker from "./ClickTracker";
 
 export class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export class App extends Component {
         <Welcome name='John' age='22' />
         <Counter initialValue={0} incrementBy={1} timeOut={1000} />
         <ClickCounter />
+        <ClickTracker />
       </div>
     );
   }
