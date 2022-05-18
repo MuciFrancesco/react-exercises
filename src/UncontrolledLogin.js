7572dd6a246147bbf040e4f25d6713254ae82961
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-18 20:47:43 +0200

    form-06

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
 export class UncontrolledLogin extends Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+  };
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const type = e.target.type;
+    const checked = e.target.checked;
+    this.setState({
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
   handleFormSubmit = (e) => {
     e.preventDefault();
     const username = e.target.elements.username.value;
@@ -16,11 +30,26 @@ export class UncontrolledLogin extends Component {
         <h3>Uncontrolled Form</h3>
         <form onSubmit={this.handleFormSubmit}>
           <label>Username:</label>
-          <input autoFocus type='text' name='username'></input>
+          <input
+            autoFocus={true}
+            value={this.state.username}
+            onChange={this.handleChange}
+            type='text'
+            name='username'
+          ></input>
           <label>Password:</label>
-          <input type='password' name='password'></input>
+          <input
+            value={this.state.password}
+            onChange={this.handleChange}
+            type='password'
+            name='password'
+          ></input>
           <input type='checkbox' name='remember'></input>
-          <button type='submit' name='btn'>
+          <button
+            type='submit'
+            name='btn'
+            disabled={!this.state.username || !this.state.password}
+          >
             Login
           </button>
           <button type='reset'>Reset</button>
