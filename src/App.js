639eed74cd55a1728c359907de84583897925469
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-19 17:10:55 +0200

    component-composition-01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Container from "./Container";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
@@ -13,15 +14,17 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Hello />
-        <Welcome name='John' age='22' />
-        <Counter initialValue={0} incrementBy={1} timeOut={1000} />
-        <ClickCounter />
-        <ClickTracker />
-        <InteractiveWelcome />
-        <Login />
-        <UncontrolledLogin />
-        <ToDoList />
+        <Container>
+          <Hello />
+          <Welcome name='John' age='22' />
+          <Counter initialValue={0} incrementBy={1} timeOut={1000} />
+          <ClickCounter />
+          <ClickTracker />
+          <InteractiveWelcome />
+          <Login />
+          <UncontrolledLogin />
+          <ToDoList />
+        </Container>
       </div>
     );
   }
