c9605a006399075741cf7ac849794ba1efbf54c1
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-20 15:37:09 +0200

    Functional-components-and-hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
 import React, { Component, createContext } from "react";
-import Container from "./Container";
-import Hello from "./Hello";
-import Welcome from "./Welcome";
-import Counter from "./Counter";
-import ClickCounter from "./ClickCounter";
-import ClickTracker from "./ClickTracker";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import Login from "./Login";
-import UncontrolledLogin from "./UncontrolledLogin";
-import ToDoList from "./ToDoList";
-import LanguageContext from "./LanguageContext";
-import DisplayLanguages from "./DisplayLanguages";
 
 export class App extends Component {
   state = {
@@ -22,51 +10,7 @@ export class App extends Component {
     });
   };
   render() {
-    return (
-      <div>
-        <Container title='My Awesome App!'>
-          <Hello />
-          <Welcome name='John' age='22' />
-          <Counter initialValue={0} incrementBy={1} timeOut={1000} />
-          <ClickCounter />
-          <ClickTracker />
-          <InteractiveWelcome />
-          <Login />
-          <UncontrolledLogin />
-          <ToDoList
-            render={(array) => {
-              const removeAtArray = (index) => {
-                const list = array;
-                list.splice(index, 1);
-                this.setState({ list });
-              };
-              return (
-                <div>
-                  <ul>
-                    {array.map((item, i) => (
-                      <>
-                        <li key={i}>{item}</li>
-                        <button onClick={() => removeAtArray(i)}>
-                          Ellimina
-                        </button>
-                      </>
-                    ))}
-                  </ul>
-                </div>
-              );
-            }}
-          />
-
-          <LanguageContext.Provider value={this.state.language}>
-            <select value={this.state.language} onChange={this.languageChange}>
-              <option value='en'>English</option>
-              <option value='it'>Italiano</option>
-            </select>
-            <DisplayLanguages />
-          </LanguageContext.Provider>
-        </Container>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
