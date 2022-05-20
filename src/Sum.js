45481f284b052fc4ae9dc66d8c2faaf3a61d34ba
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-20 16:05:24 +0200

    Functional-components-03

--- src/Sum.js
+++ src/Sum.js
@@ -3,7 +3,10 @@ import React from "react";
 function Sum({ summOfNumber }) {
   return (
     <div>
-      <h1>This is the number: {summOfNumber}</h1>
+      <h1>
+        This is the number:{" "}
+        {summOfNumber || "i'dont have a number for you,sorry"}
+      </h1>
     </div>
   );
 }
