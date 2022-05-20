ce5b363d9dda068bb5a6ce8ee1a0efe58f44ddf0
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-20 16:00:00 +0200

    Functional-components-02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Welcome from "./Welcome";
-export class App extends Component {
-  render() {
-    return (
-      <>
-        <Welcome name='andra' />
-      </>
-    );
-  }
+import Sum from "./Sum";
+
+const arrayOfNumber = [1, 2, 4, 6, 8, 10, 22, 55, 77];
+const summOfNumber = arrayOfNumber.reduce((acc, el) => acc + el, 0);
+
+function App() {
+  return (
+    <div className='App'>
+      <Welcome name='Andrea' />
+      <Sum summOfNumber={summOfNumber} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sum.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sum({ summOfNumber }) {
+  return (
+    <div>
+      <h1>This is the number: {summOfNumber}</h1>
+    </div>
+  );
+}
+
+export default Sum;
