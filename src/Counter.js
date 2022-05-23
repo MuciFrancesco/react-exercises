e014b985824fb20b900472541b99326fd87a5076
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-23 20:25:16 +0200

    constom-hooks-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,5 @@
-import React, { useState, useCallback, useEffect } from "react";
-
-function useCounter(initialValue = 0) {
-  const [counter, setCounter] = useState(initialValue);
-  const counterIncrement = useCallback(() => {
-    setCounter(counter + 1);
-  }, [counter]);
-
-  const counterDecrement = useCallback(() => {
-    setCounter(counter - 1);
-  }, [counter]);
-
-  const counterReset = () => {
-    setCounter(initialValue);
-  };
-  useEffect(() => {
-    console.log(counter);
-  }, [counter]);
-
-  return {
-    counter: counter,
-    counterIncrement: counterIncrement,
-    counterDecrement: counterDecrement,
-    counterReset: counterReset,
-  };
-}
+import React from "react";
+import useCounter from "./useCounter";
 
 function Counter({ initialValue = 0 }) {
   const { counter, counterIncrement, counterDecrement, counterReset } =
---
+++ src/useCounter.js
@@ -0,0 +1,26 @@
+import React, { useState, useCallback, useEffect } from "react";
+function useCounter(initialValue = 0) {
+  const [counter, setCounter] = useState(initialValue);
+  const counterIncrement = useCallback(() => {
+    setCounter(counter + 1);
+  }, [counter]);
+
+  const counterDecrement = useCallback(() => {
+    setCounter(counter - 1);
+  }, [counter]);
+
+  const counterReset = () => {
+    setCounter(initialValue);
+  };
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
+  return {
+    counter: counter,
+    counterIncrement: counterIncrement,
+    counterDecrement: counterDecrement,
+    counterReset: counterReset,
+  };
+}
+export default useCounter;
