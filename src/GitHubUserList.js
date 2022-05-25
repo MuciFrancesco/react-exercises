575eed17e3d48ccd8f119be927583d971f0030ab
MuciFrancesco <91052791+MuciFrancesco@users.noreply.github.com>
2022-05-25 12:10:45 +0200

    reviewed

--- src/GitHubUserList.js
+++ src/GitHubUserList.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import GitHubUsers from "./GitHubUsers";
 
-function GitHubUserList() {
-  return <div></div>;
+function GitHubUserList({ userList, loading, error, data }) {
+  return (
+    <div>
+      {userList.map((user, i) => (
+        <GitHubUsers
+          key={i}
+          loading={loading}
+          error={error}
+          data={data}
+          username={user}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default GitHubUserList;
--- src/GitHubUsers.js
+++ src/GitHubUsers.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function GitHubUsers({ data, loading, error }) {
+function GitHubUsers({ username, data, loading, error }) {
   return (
     <div>
       {loading && <h1>loading...</h1>}
       {error && <h1>Sorry we have a problem !</h1>}
       {data && (
         <div>
-          <h1>{data.name}</h1>
-          <img src={data.avatar_url} alt='img-profile' />
-          <p>{data.bio}</p>
+          <h1>{username.name || "user not found"}</h1>
+          <img src={username.avatar_url || "img not found"} alt='img-profile' />
+          <p>{username.bio || "bio not found "}</p>
           <button>
-            <a href={data.html_url} target='_blank' rel='noreferrer'>
+            <a href={username.html_url} target='_blank' rel='noreferrer'>
               Visita il mio GitHub
             </a>
           </button>
